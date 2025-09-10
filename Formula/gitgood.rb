25dc4402a202ab924b77059144daba183131b7b8
Adrian Marten <Adrian.marten@elaway.io>
2025-09-10 22:28:15 +0200

    chore(formula): bump to v0.1.3

--- Formula/gitgood.rb
+++ Formula/gitgood.rb
@@ -5,19 +5,19 @@ class Gitgood < Formula
 
   on_macos do
     on_intel do
-      url "https://github.com/duhman/gitgood-cli/releases/download/v0.1.2/gitgood-cli-v0.1.2-macos-x64.tar.gz"
-      sha256 "cb00db5eab9753d30212fdfd5203c7203a0e9c22bb3c870495a75c8e9c848515"
+      url "https://github.com/duhman/gitgood-cli/releases/download/v0.1.3/gitgood-cli-v0.1.3-macos-x64.tar.gz"
+      sha256 "39530dd194d8672baeef7340cd4d6740179b473394047817c50eb842bdea0096"
     end
     on_arm do
-      url "https://github.com/duhman/gitgood-cli/releases/download/v0.1.2/gitgood-cli-v0.1.2-macos-arm64.tar.gz"
-      sha256 "85e313f832567b7d8535d5445259503bdd37eaed4bc34feb12083698fb7fc7fd"
+      url "https://github.com/duhman/gitgood-cli/releases/download/v0.1.3/gitgood-cli-v0.1.3-macos-arm64.tar.gz"
+      sha256 "123f75dcef62df9aa8179bea3eea301404ab836d6fbecf9ef9e55e3da55a3f0a"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/duhman/gitgood-cli/releases/download/v0.1.2/gitgood-cli-v0.1.2-linux-x64.tar.gz"
-      sha256 "39429e5ba2c2c977168f1491089e07e69b31537716df33a34997f8e8202c8727"
+      url "https://github.com/duhman/gitgood-cli/releases/download/v0.1.3/gitgood-cli-v0.1.3-linux-x64.tar.gz"
+      sha256 "1e00fa301108c60ee7623d76667c87c1a241a6526430f24299ed401c615705e0"
     end
   end
 
