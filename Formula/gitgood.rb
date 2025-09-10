c0ab35aadf641fadf6facc7942c9abb2ea90956b
Adrian Marten <Adrian.marten@elaway.io>
2025-09-10 22:22:45 +0200

    chore(formula): v0.1.1 include dist via libexec wrapper

--- Formula/gitgood.rb
+++ Formula/gitgood.rb
@@ -5,27 +5,32 @@ class Gitgood < Formula
 
   on_macos do
     on_intel do
-      url "https://github.com/duhman/gitgood-cli/releases/download/v0.1.0/gitgood-cli-v0.1.0-macos-x64.tar.gz"
-      sha256 "258adef1071b9fb740975eefcee7baccfa408070c1698d44d301d622a4b00538"
+      url "https://github.com/duhman/gitgood-cli/releases/download/v0.1.1/gitgood-cli-v0.1.1-macos-x64.tar.gz"
+      sha256 "63708712867f080fde8d3263de4e65a2418d1ce2d4842345a7fc745d94c3c84e"
     end
     on_arm do
-      url "https://github.com/duhman/gitgood-cli/releases/download/v0.1.0/gitgood-cli-v0.1.0-macos-arm64.tar.gz"
-      sha256 "5f30e90d3486f419d695dd1b9b607347a290b548da2bfe447873a0cf9e89c4fd"
+      url "https://github.com/duhman/gitgood-cli/releases/download/v0.1.1/gitgood-cli-v0.1.1-macos-arm64.tar.gz"
+      sha256 "236391d68c4d991a17aeb32a68c81aa502864687362d609376750afc8f2f1b6d"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/duhman/gitgood-cli/releases/download/v0.1.0/gitgood-cli-v0.1.0-linux-x64.tar.gz"
-      sha256 "b34202fb994e3906b23b7f3c273cdd26af27eaba13b92b7ad2d0fadb8def2c93"
+      url "https://github.com/duhman/gitgood-cli/releases/download/v0.1.1/gitgood-cli-v0.1.1-linux-x64.tar.gz"
+      sha256 "39422d91cfc3a83f7646e57ec00e2cf317c25734c9ccea519482d5b305059ae8"
     end
   end
 
   def install
-    bin.install "gitgood"
+    libexec.install Dir["*"]
+    (bin/"gitgood").write <<~EOS
+      #!/bin/bash
+      exec node "{libexec}/dist/index.js" "$@"
+    EOS
+    (bin/"gitgood").chmod 0755
   end
 
   test do
-    system "#{bin}/gitgood", "--version"
+    system "{bin}/gitgood", "--version"
   end
 end
